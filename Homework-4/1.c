90f910d395e2d507c37b50f7619d1c6bd84aea3d
Parham Alvani <parham.alvani@gmail.com>
2014-11-02 18:26:43 +0330

    Homework-4, improved.

--- Homework-4/1.c
+++ Homework-4/1.c
@@ -10,14 +10,25 @@ sem_t reader_sem;
 int shm_fd;
 
 int main(int argc, char* argv[]){
+	shm_unlink("/tmp");
 	shm_fd = shm_open("/tmp", O_RDWR | O_CREAT, 0666);
 	FILE* shm_file = fdopen(shm_fd, "r+");
-	fprintf(shm_file, "HELLO_WORLD");
+	fprintf(shm_file, "HELLO_WORLD\n");
+	fflush(shm_file);
+	printf("%d\n", shm_fd);
 
-	if(fork() == 0){
-		FILE* shm_file = fdopen(shm_fd, "r+");
-		char buffer[255];
-		fscanf(shm_file, "%s", buffer);
-		printf("%s\n", buffer);
+	int i = 0;
+	for(i = 0; i < 10; i++){
+		if(fork() == 0){
+			printf("%d\n", shm_fd);
+			FILE* shm_file = fdopen(shm_fd, "r+");
+			char buffer[255];
+			rewind(shm_file);
+			fscanf(shm_file, "%s", buffer);
+			printf("%s\n", buffer);
+			exit(0);
+		}
 	}
+	wait();
+	close(shm_fd);
 }
