7910f1d3b8b7070a1d0b8cfa47cae38dfff63961
Parham Alvani <parham.alvani@gmail.com>
2014-11-02 13:55:05 +0330

    some dummy code is added.

--- Homework-4/1.c
+++ Homework-4/1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <semaphore.h>
 #include <sys/mman.h>
 #include <sys/types.h>
@@ -10,4 +11,13 @@ int shm_fd;
 
 int main(int argc, char* argv[]){
 	shm_fd = shm_open("/tmp", O_RDWR | O_CREAT, 0666);
+	FILE* shm_file = fdopen(shm_fd, "r+");
+	fprintf(shm_file, "HELLO_WORLD");
+
+	if(fork() == 0){
+		FILE* shm_file = fdopen(shm_fd, "r+");
+		char buffer[255];
+		fscanf(shm_file, "%s", buffer);
+		printf("%s\n", buffer);
+	}
 }
