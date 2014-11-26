025ea5fa5a93c0bba7e1fa8106c2a08ad8490305
Parham Alvani <parham.alvani@gmail.com>
2014-11-26 11:39:51 +0330

    memory object bug fixed.

--- Homework-5/memory.c
+++ Homework-5/memory.c
@@ -7,6 +7,7 @@ void mem_write(uint8_t frame, uint8_t offset, uint8_t input){
 
 uint8_t mem_read(uint8_t frame, uint8_t offset){
 	FILE* memory = fopen("memory", "r");
+	fseek(memory, frame * 256 + offset, SEEK_SET);
 	uint8_t ret;
 	fread(&ret, sizeof(uint8_t), 1, memory);
 	return ret;
