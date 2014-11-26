c873a34dc3ede28c69a8743d06493661ecc1663d
Parham Alvani <parham.alvani@gmail.com>
2014-11-26 08:52:09 +0330

    page_table object improved.
    page_tabel object now can handle demand paging.

--- Homework-5/page_table.c
+++ Homework-5/page_table.c
@@ -7,5 +7,10 @@ void add_page_table_entry(uint8_t index, page_table_entry* entry){
 	table[index] = *entry;
 }
 uint8_t get_base_addr(uint8_t index){
-	return table[index].base;
+	if(table[index].vn = 1){
+		return table[index].base;
+	}else{
+		handler(table[index].base);
+		return table[index].base;
+	}
 }
--- Homework-5/page_table.h
+++ Homework-5/page_table.h
@@ -4,16 +4,27 @@
 #include <stdint.h>
 
 // Page table entry structure
-//  8               0
-// |-----------------|
-// | Physical  Base  |
-// |     Address     |
-// |-----------------|
+// 16           8 7               0
+// |-----------|-|-----------------|
+// | reserved  |V| Physical  Base  |
+// |           |N|     Address     |
+// |-----------|-|-----------------|
+//
+// * VN : Valid/Invalid
 //
 
 typedef struct{
 	uint8_t base;
-} page_table_entry;
+	uint8_t vn:1;
+} __attribute__((packed)) page_table_entry;
+
+// Page fault handler must set for handling page faults.
+//
+//
+
+typedef void (*page_fault_handler)(uint8_t base);
+
+page_fault_handler handler;
 
 void add_page_table_entry(uint8_t index, page_table_entry* entry);
 uint8_t get_bsae_addr(uint8_t index);
