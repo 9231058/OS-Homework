19b4b44b526bd57fc6be772500a8a54248d18a33
Parham Alvani <parham.alvani@gmail.com>
2014-12-10 02:58:19 +0330

    script added to compress.sh.

--- Homework-7/compress.sh
+++ Homework-7/compress.sh
@@ -2,4 +2,4 @@
 HW_NO=7
 
 rm os_assigment$[HW_NO]_group20.tgz
-tar cvfz os_assigment$[HW_NO]_group20.tgz main.c fifo.h Makefile note.txt
+tar cvfz os_assigment$[HW_NO]_group20.tgz main.c fifo.h Makefile fifo.load fifo.unload note.txt
