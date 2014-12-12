c1ecde54b9dcecc15273bc2c3bc02b298ecdb47f
Parham Alvani <parham.alvani@gmail.com>
2014-12-12 23:47:27 +0330

    Homework-6, problem 1 is started.

---
+++ Homework-6/1/hdd.c
@@ -0,0 +1,15 @@
+// In The Name Of God
+// ========================================
+// * File Name : hdd.c
+// 
+// * Creation Date : 12-12-2014
+//
+// * Last Modified : Fri 12 Dec 2014 11:46:40 PM IRST
+//
+// * Created By : Parham Alvani (parham.alvani@gmail.com)
+// =======================================
+static int head;
+
+void set_head(int position){
+	head = position;
+}
---
+++ Homework-6/1/hdd.h
@@ -0,0 +1,12 @@
+// In The Name Of God
+// ========================================
+// * File Name : hdd.h
+// 
+// * Creation Date : 12-12-2014
+//
+// * Last Modified : Fri 12 Dec 2014 11:46:43 PM IRST
+//
+// * Created By : Parham Alvani (parham.alvani@gmail.com)
+// =======================================
+void set_head(int position);
+
