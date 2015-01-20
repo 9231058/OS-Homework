84e9eb3945c503474b8e522496ac66223c415f9d
Parham Alvani <parham.alvani@gmail.com>
2015-01-20 08:35:51 +0330

    Some issue in corrected in Homework-6
    Former-commit-id: ef8f2874fbdc3dc11d9baba6df5307cd1e921dd3

--- Homework-6/1/hdd.c
+++ Homework-6/1/hdd.c
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 20-01-2015
  *
- * [] Last Modified : Tue 20 Jan 2015 08:25:09 AM IRST
+ * [] Last Modified : Tue 20 Jan 2015 08:35:40 AM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -24,17 +24,18 @@ void set_head(int position)
 
 int fcfs(int req[], int n)
 {
-	#ifdef SHOW	
+	#ifdef SHOW
 		show_header();
 	#endif
 
 	int index = 0;
 	int seek = 0;
-	while(index != n){	
+
+	while (index != n) {
 		seek += abs(head - req[index]);
 		head = req[index];
 		index++;
-		
+
 		#ifdef SHOW
 			show_head(head);
 		#endif
@@ -55,12 +56,14 @@ int sstf(int req[], int n)
 	int seek = 0;
 	int done = 0;
 	int single_done[n];
+
 	memset(&single_done, 0, n * sizeof(int));
-	while(done != n){
+	while (done != n) {
 		int min = MAX_CYLINDERS;
 		int min_index = -1;
-		for(int i = 0; i < n; i++){
-			if(!single_done[i] && min > abs(head - req[i])){
+
+		for (int i = 0; i < n; i++) {
+			if (!single_done[i] && min > abs(head - req[i])) {
 				min = abs(head - req[i]);
 				min_index = i;
 			}
@@ -91,6 +94,7 @@ int scan(int req[], int n)
 	int done = 0;
 	int single_done[n];
 	int direction = 0;
+
 	memset(&single_done, 0, n * sizeof(int));
 	while (done != n) {
 		for (int i = 0; i < n; i++) {
@@ -104,7 +108,7 @@ int scan(int req[], int n)
 			}
 		}
 		seek++;
-		if(direction) {
+		if (direction) {
 			head++;
 			if (head == MAX_CYLINDERS) {
 				head -= 2;
@@ -112,7 +116,7 @@ int scan(int req[], int n)
 			}
 		} else {
 			head--;
-			if(head == -1) {
+			if (head == -1) {
 				head += 2;
 				direction = 1;
 			}
@@ -121,7 +125,7 @@ int scan(int req[], int n)
 	#ifdef SHOW
 		show_footer();
 	#endif
-	
+
 	return seek;
 }
 
@@ -134,10 +138,11 @@ int c_scan(int req[], int n)
 	int seek = 0;
 	int done = 0;
 	int single_done[n];
+
 	memset(&single_done, 0, n * sizeof(int));
-	while(done != n){
-		for(int i = 0; i < n; i++){
-			if((!single_done[i]) && req[i] == head){
+	while (done != n) {
+		for (int i = 0; i < n; i++) {
+			if ((!single_done[i]) && req[i] == head) {
 				#ifdef SHOW
 					show_head(head);
 				#endif
@@ -162,48 +167,50 @@ int look(int req[], int n)
 	#ifdef SHOW
 		show_header();
 	#endif
-	
+
 	int seek = 0;
 	int done = 0;
 	int single_done[n];
 	int direction = 0;
+
 	memset(&single_done, 0, n * sizeof(int));
-	while(done != n){
-		for(int i = 0; i < n; i++){
-			if((!single_done[i]) && req[i] == head){
+	while (done != n) {
+		for (int i = 0; i < n; i++) {
+			if ((!single_done[i]) && req[i] == head) {
 				#ifdef SHOW
 					show_head(head);
 				#endif
-				
+
 				single_done[i] = 1;
 				done++;
 			}
 		}
 		int find = 0;
-		if(direction){
-			for(int i = 0; i < n; i++){
-				if((!single_done[i]) && req[i] > head){
+
+		if (direction) {
+			for (int i = 0; i < n; i++) {
+				if ((!single_done[i]) && req[i] > head) {
 					find = 1;
 					seek++;
 					head++;
 					break;
 				}
 			}
-			if(!find){
+			if (!find) {
 				direction = 0;
 				seek++;
 				head--;
 			}
-		}else{
-			for(int i = 0; i < n; i++){
-				if((!single_done[i]) && req[i] < head){
+		} else {
+			for (int i = 0; i < n; i++) {
+				if ((!single_done[i]) && req[i] < head) {
 					find = 1;
 					seek++;
 					head--;
 					break;
 				}
 			}
-			if(!find){
+			if (!find) {
 				direction = 1;
 				seek++;
 				head--;
@@ -226,10 +233,11 @@ int c_look(int req[], int n)
 	int seek = 0;
 	int done = 0;
 	int single_done[n];
+
 	memset(&single_done, 0, n * sizeof(int));
-	while(done != n){
-		for(int i = 0; i < n; i++){
-			if((!single_done[i]) && req[i] == head){
+	while (done != n) {
+		for (int i = 0; i < n; i++) {
+			if ((!single_done[i]) && req[i] == head) {
 				#ifdef SHOW
 					show_head(head);
 				#endif
@@ -240,23 +248,25 @@ int c_look(int req[], int n)
 		}
 		seek++;
 		int find = 0;
-		for(int i = 0; i < n; i++){
-			if((!single_done[i]) && req[i] > head){
+
+		for (int i = 0; i < n; i++) {
+			if ((!single_done[i]) && req[i] > head) {
 				find = 1;
 				head++;
 				break;
 			}
 		}
-		if(!find){
+		if (!find) {
 			int min = MAX_CYLINDERS;
 			int min_index = -1;
-			for(int i = 0; i < n; i++){
-				if(!single_done[i] && min > req[i]){
+
+			for (int i = 0; i < n; i++) {
+				if (!single_done[i] && min > req[i]) {
 					min = req[i];
 					min_index = i;
 				}
 			}
-			if(min_index != -1)
+			if (min_index != -1)
 				head = req[min_index];
 		}
 	}
