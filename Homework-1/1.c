fc4b38fffd7bda65b579ee0665563a67c96a99bc
Parham Alvani <parham.alvani@gmail.com>
2015-02-06 22:41:21 +0330

    Code formatting is corrected.
    Former-commit-id: c61e0ff8c974bb77382aaf6cd37abc784617fa70

--- Homework-1/1.c
+++ Homework-1/1.c
@@ -1,68 +1,82 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : 1.c
+ *
+ * [] Creation Date : 06-02-2015
+ *
+ * [] Last Modified : Fri Feb  6 22:41:15 2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-void die(const char* msg);
+void die(const char *msg)
+{
+	perror(msg);
+	exit(1);
+}
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 	int fds[2];
 	pid_t child;
+	const char *path = "/";
 
-	const char* path = "/";
-	if(argc > 1){
+	if (argc > 1)
 		path = argv[1];
-	}
-
-	if(pipe(fds) == -1){
+	if (pipe(fds) == -1)
 		die("pipe()");
-	}
-
 	child = fork();
-	if(child == -1){
+	if (child == -1) {
 			die("fork()");
-	}else if(child == 0){
+	} else if (child == 0) {
 			close(1);
-			close(fds[0]); //very very important if u remove it, program does not end.
+			/*
+			 * very very important if u
+			 * remove it, program does not end.
+			*/
+			close(fds[0]);
 
 			fprintf(stderr, "INFO: [child] child == %d\n", child);
 
-			if(dup(fds[1]) == -1){
+			if (dup(fds[1]) == -1)
 				die("dup()");
-			}
-		
+
 			fprintf(stderr, "INFO: [child] ls is going to exec\n");
 
-			if(execlp("ls", "ls", "-la", path, NULL) == -1){
+			if (execlp("ls", "ls", "-la", path, NULL) == -1)
 				die("execlp()");
-			}
-	}else{
+	} else {
 			close(0);
-			close(fds[1]);	//very very important if u remove it, program does not end.
-			
+			/*
+			 * very very important if u
+			 * remove it, program does not end.
+			*/
+			close(fds[1]);
+
 			fprintf(stderr, "INFO: [parent] child == %d\n", child);
-			
-			fprintf(stderr, "INFO: [parent] I am going to wait\n");			
+			fprintf(stderr, "INFO: [parent] I am going to wait\n");
+
 			int status = 0;
-			if(wait(&status) != child){
+
+			if (wait(&status) != child)
 				die("wait()");
-			}
+
 			fprintf(stderr, "INFO: [parent] wait complete\n");
-			fprintf(stderr, "INFO: [parent] Exited: %d - Status: %d\n",WIFEXITED(status),WEXITSTATUS(status));
+			fprintf(stderr, "INFO: [parent] Exited: %d Status: %d\n",
+					WIFEXITED(status), WEXITSTATUS(status));
 
-			if(dup(fds[0]) == -1){
+			if (dup(fds[0]) == -1)
 				die("dup()");
-			}
 
-			if(execlp("wc", "wc", "-l", NULL) == -1){
+			if (execlp("wc", "wc", "-l", NULL) == -1)
 				die("execlp()");
-			}
 
 			exit(0);
 	}
 	return 0;
-}
-
-void die(const char* msg){
-	perror(msg);
-	exit(1);
 }
--- Homework-1/2.c
+++ Homework-1/2.c
@@ -1,3 +1,15 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : 2.c
+ *
+ * [] Creation Date : 06-02-2015
+ *
+ * [] Last Modified : Fri Feb  6 22:39:39 2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,70 +19,72 @@
 
 const int MAX_BUFFER_SIZE = 1024;
 
-void die(const char* msg);
+void die(const char *msg)
+{
+	perror(msg);
+	exit(1);
+}
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 	pid_t child;
+	const char *path = "file.txt";
 
-	const char* path = "file.txt";
-	if(argc > 2){
+	if (argc > 2)
 		path = argv[1];
-	}
 
 	child = fork();
-	if(child == -1){
+	if (child == -1) {
 		die("fork()");
-	}else if(child == 0){
-		mqd_t mqd = mq_open("/file_message_queue", O_RDONLY); // using / is necessary
-		if(mqd == -1){
+	} else if (child == 0) {
+		/* using / is necessary */
+		mqd_t mqd = mq_open("/file_message_queue", O_RDONLY);
+
+		if (mqd == -1)
 			die("[child] mq_open()");
-		}
 
 		int number = 0;
 		char buffer[MAX_BUFFER_SIZE];
-		while(1){
-			int size;
-			if((size = mq_receive(mqd, buffer, MAX_BUFFER_SIZE, 0)) == -1){
+
+		while (1) {
+			int size = mq_receive(mqd, buffer, MAX_BUFFER_SIZE, 0);
+
+			if (size == -1)
 				die("mq_receive()");
-			}
 			buffer[size] = 0;
-			if(strcmp(buffer, "FIN") != 0){
+			if (strcmp(buffer, "FIN") != 0) {
 				number++;
-			}else{
+			} else {
 				printf("%d\n", number);
 				mq_unlink("/file_message_queue");
 				exit(0);
 			}
 		}
-	}else{
+	} else {
 		struct mq_attr attr;
+
 		attr.mq_flags = 0;
 		attr.mq_maxmsg = 10;
 		attr.mq_msgsize = MAX_BUFFER_SIZE;
 		attr.mq_curmsgs = 0;
-		mqd_t mqd = mq_open("/file_message_queue", O_CREAT | O_WRONLY, 0644, &attr); // using / is necessary
-		if(mqd == -1){
-			die("[parent] mq_open()");
-		}
+		/* using / is necessary */
+		mqd_t mqd = mq_open("/file_message_queue",
+				O_CREAT | O_WRONLY, 0644, &attr);
 
-		FILE* file = fopen(path, "r");
+		if (mqd == -1)
+			die("[parent] mq_open()");
+
+		FILE *file = fopen(path, "r");
 		char buffer[MAX_BUFFER_SIZE];
-		while(fscanf(file, "%s", buffer) > 0){
-			if(mq_send(mqd, buffer, strlen(buffer), 0) == -1){
+
+		while (fscanf(file, "%s", buffer) > 0)
+			if (mq_send(mqd, buffer, strlen(buffer), 0) == -1)
 				die("mq_send()");
-			}
-		}
 		fclose(file);
 		strcpy(buffer, "FIN");
-		if(mq_send(mqd, buffer, strlen(buffer), 0) == -1){
+		if (mq_send(mqd, buffer, strlen(buffer), 0) == -1)
 			die("mq_send()");
-		}
 		mq_close(mqd);
 	}
 	return 0;
-}
-
-void die(const char* msg){
-	perror(msg);
-	exit(1);
 }
