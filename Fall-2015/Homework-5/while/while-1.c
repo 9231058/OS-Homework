bbe088874930f210384df5a6291e79dc7b1964b5
Parham Alvani <parham.alvani@gmail.com>
2015-11-13 15:42:57 +0330

    Homework-5 was added.

---
+++ Fall-2015/Homework-5/while/while-1.c
@@ -0,0 +1,24 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : while-1.c
+ *
+ * [] Creation Date : 13-11-2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2015 Parham Alvani.
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while (1) {
+		i++;
+		printf("%d\n", i);
+	}
+	return 0;
+}
---
+++ Fall-2015/Homework-5/while/while-2.c
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : while-2.c
+ *
+ * [] Creation Date : 13-11-2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2015 Parham Alvani.
+*/
+#include <stdio.h>
+#include <sched.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+}
