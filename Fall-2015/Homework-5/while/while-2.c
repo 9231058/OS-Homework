4a443f58834cb1f7110472d8b41828e4cef50adc
Parham Alvani <parham.alvani@gmail.com>
2015-11-13 15:55:57 +0330

    While-2 was completed.

--- Fall-2015/Homework-5/while/while-2.c
+++ Fall-2015/Homework-5/while/while-2.c
@@ -11,10 +11,23 @@
 /*
  * Copyright (c) 2015 Parham Alvani.
 */
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <sched.h>
 
 int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i, ret;
+	cpu_set_t set;
+
+	CPU_SET(0, &set);
+	ret = sched_setaffinity(0, sizeof(cpu_set_t), &set);
+	if (ret == -1)
+		perror("sched_setaffinity()");
+	i = 0;
+	while (1) {
+		i++;
+		printf("%d\n", i);
+	}
 }
